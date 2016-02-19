376b55eb59a985e7b9ba1df138912576e34bed50
Cheng Peng <zx603852402@vip.qq.com>
2016-02-19 16:26:45 +0000

    0.5 Version

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -47,10 +47,6 @@ module.exports = function(socketio) {
 
     socket.connectedAt = new Date();
 
-    socket.log = function(...data) {
-      console.log(`SocketIO ${socket.nsp.name} [${socket.address}]`, ...data);
-    };
-
     // Call onDisconnect.
     socket.on('disconnect', () => {
       onDisconnect(socket);
