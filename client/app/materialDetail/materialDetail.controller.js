43dc715d882d2d21883ace83e66a4dd61c4f675f
Scorpio <scorpiocoffee91@gmail.com>
2016-04-23 16:06:54 +0100

    scorpio

--- client/app/materialDetail/materialDetail.controller.js
+++ client/app/materialDetail/materialDetail.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('maxiProjectApp')
-  .controller('MaterialDetailCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MaterialDetailCtrl', function ($scope,ipCookie) {
+    $scope.email = function() {
+    	if(ipCookie('Login')) {
+    		Material.getMaterial(ipCookie('Login').material_id).success(function(ms) {
+    			$scope.info = ipCookie('Login');
+    			$scope.ms = ms.name;
+    			$location.path('mailto:zx603852402@gmail.com');
+    		}).error(function(err) {
+    			
+    		});
+    	}
+    	else {
+    		alert("Please login first!");
+    	}
+    };
   });
