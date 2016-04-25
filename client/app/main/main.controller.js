3d864e1b2755faa701eaefadb3b00f1b876b5097
Cheng Peng <zx603852402@vip.qq.com>
2016-04-25 13:20:13 +0100

    main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -98,28 +98,5 @@ angular.module('maxiProjectApp')
           }
         }
       };
-      $scope.allnews = 
-      [
-        {date:"1992.05.08",
-         title:"Hello this is a demo",
-         content:"The contents",
-         id:0},
-         {date:"1992.05.08",
-         title:"Hello this is a demo",
-         content:"The contents",
-         id:1},
-         {date:"1992.05.08",
-         title:"Hello this is a demo",
-         content:"The contents",
-         id:2},
-         {date:"1992.05.08",
-         title:"Hello this is a demo",
-         content:"The contents",
-         id:3},
-         {date:"1992.05.08",
-         title:"Hello this is a demo",
-         content:"The contents",
-         id:4}
-      ];
     });
 });
